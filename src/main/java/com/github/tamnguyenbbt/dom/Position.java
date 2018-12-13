2d23244d99075d332266abb94b2030dc2bb11ddd
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-13 17:53:20 +1100

    Create Position.java

---
+++ src/main/java/com/github/tamnguyenbbt/dom/Position.java
@@ -0,0 +1,16 @@
+package com.github.tamnguyenbbt.dom;
+
+import java.util.ArrayList;
+
+final class Position extends ArrayList<Integer>
+{
+    protected Position()
+    {
+        super();
+    }
+
+    protected Position(Position position)
+    {
+        super(position);
+    }
+}
