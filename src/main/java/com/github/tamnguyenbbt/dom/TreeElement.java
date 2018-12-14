4591d1d9ea59129b310c90bac9971c05e22b3c99
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-14 22:44:51 +1100

    Add more properties to support top down algorithm

--- src/main/java/com/github/tamnguyenbbt/dom/TreeElement.java
+++ src/main/java/com/github/tamnguyenbbt/dom/TreeElement.java
@@ -1,21 +1,34 @@
-package com.github.tamnguyenbbt.dom;
-
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Element;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public final class TreeElement
 {
+    protected UUID id;
     protected Element element;
     protected Position position;
-    protected String ownText;
-    protected boolean isAnchorCandidate;
+    protected boolean asAnchorCandidate;
     protected List<TreeElement> elementsWithSameOwnText;
+    protected Map<TreeElement,Integer> distancesToAnchors;
+    protected Map<TreeElement,Position> rootPositionsForAnchors;
+    protected Map<TreeElement,Attribute> linkedAnchors;
+    protected List<String> uniqueXpaths;
+    protected List<String> uniqueXpathsWithAttributes;
+    protected List<String> leastRefactoredXpaths;
+    protected List<String> leastRefactoredXpathsWithAttributes;
 
     protected TreeElement()
     {
+        id = UUID.randomUUID();
         position = new Position();
         elementsWithSameOwnText = new ArrayList<>();
+        distancesToAnchors = new HashMap<>();
+        linkedAnchors = new HashMap<>();
+        rootPositionsForAnchors = new HashMap<>();
+        uniqueXpaths = new ArrayList<>();
+        uniqueXpathsWithAttributes = new ArrayList<>();
+        leastRefactoredXpaths = new ArrayList<>();
+        leastRefactoredXpathsWithAttributes = new ArrayList<>();
     }
 
     protected TreeElement(Element element)
