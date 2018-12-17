76fb688f7970f343173775fbc4cae4157d173210
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-17 20:55:38 +1100

    Update Util.java

--- src/main/java/com/github/tamnguyenbbt/dom/Util.java
+++ src/main/java/com/github/tamnguyenbbt/dom/Util.java
@@ -1,9 +1,10 @@
 package com.github.tamnguyenbbt.dom;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
-public class Util
+class Util
 {
     protected static <K,V> boolean hasItem(Map<K,V> map)
     {
@@ -18,5 +19,18 @@ public class Util
     protected static <T> boolean hasNoItem(List<T> list)
     {
         return (list == null || list.isEmpty());
+    }
+
+    protected static String removeLineSeparators(String input)
+    {
+        String separator = File.separator;
+        return input == null ? null : input.replace(separator, "").replace("\n", "").replace("\r", "");
+    }
+
+    protected static String cutText(String input, int toLength, boolean removeLineSeparators)
+    {
+        String output =  input == null ? null : input.substring(0, Math.min(input.length(), toLength));
+        output = input.length() > toLength ? output + "..." : output;
+        return removeLineSeparators ? removeLineSeparators(output) : output;
     }
 }
