1f81d35c2472674ef8f4a847bc505203e11057ce
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-17 20:55:02 +1100

    Update TreeElement.java

--- src/main/java/com/github/tamnguyenbbt/dom/TreeElement.java
+++ src/main/java/com/github/tamnguyenbbt/dom/TreeElement.java
@@ -19,7 +19,7 @@ final class TreeElement
     protected List<String> leastRefactoredXpaths;
     protected List<String> leastRefactoredXpathsWithAttributes;
 
-    protected TreeElement()
+    protected TreeElement(Element element)
     {
         id = UUID.randomUUID();
         position = new Position();
@@ -31,12 +31,30 @@ final class TreeElement
         uniqueXpathsWithAttributes = new ArrayList<>();
         leastRefactoredXpaths = new ArrayList<>();
         leastRefactoredXpathsWithAttributes = new ArrayList<>();
+        this.element = element;
+
+        if(element != null)
+        {
+            this.element.attr(Tree.uniqueInsertedAttribute, id.toString());
+        }
     }
 
-    protected TreeElement(Element element)
+    @Override
+    public boolean equals(Object obj)
     {
-        this();
-        this.element = element;
+        if(obj == null || (obj != null && !(obj instanceof TreeElement)))
+        {
+            return false;
+        }
+
+        if(this.element == null || ((TreeElement) obj).element == null)
+        {
+            return false;
+        }
+
+        return this.element.hasAttr(Tree.uniqueInsertedAttribute)
+               && ((TreeElement) obj).element.hasAttr(Tree.uniqueInsertedAttribute)
+               && this.element.attr(Tree.uniqueInsertedAttribute).equals(((TreeElement) obj).element.attr(Tree.uniqueInsertedAttribute));
     }
 
     protected void setElementsWithSameOwnText(List<TreeElement> elementsWithSameOwnText)
@@ -50,10 +68,10 @@ final class TreeElement
     protected void setAsAnchorCandidate(List<TreeElement> elementsWithSameOwnText)
     {
         if(Util.hasItem(elementsWithSameOwnText) &&
-                elementsWithSameOwnText.size() == 1 &&
-                element != null &&
-                element.ownText() != null &&
-                !element.ownText().trim().equals(""))
+           elementsWithSameOwnText.size() == 1 &&
+           element != null &&
+           element.ownText() != null &&
+           !element.ownText().trim().equals(""))
         {
             asAnchorCandidate = true;
         }
@@ -78,6 +96,56 @@ final class TreeElement
                 }
             });
         }
+    }
+
+    protected List<String> attachIdAndNameToXpaths(List<String> xpaths)
+    {
+        List<String> result = new ArrayList<>();
+
+        if(element != null && Util.hasItem(xpaths))
+        {
+            xpaths.forEach(x -> {
+                String newXpath = attachIdToXpath(x);
+                newXpath = attachNameToXpath(newXpath);
+                result.add(newXpath);
+            });
+        }
+
+        return result;
+    }
+
+    protected String attachIdToXpath(String xpath)
+    {
+        String xpathWithAttributes = xpath;
+
+        if(xpath != null && element != null)
+        {
+            Attribute idAttribute = getAttributeByName("id");
+
+            if(idAttribute != null)
+            {
+                xpathWithAttributes = String.format("%s[@id='%s']", xpathWithAttributes, idAttribute.getValue());
+            }
+        }
+
+        return xpathWithAttributes;
+    }
+
+    protected String attachNameToXpath(String xpath)
+    {
+        String xpathWithAttributes = xpath;
+
+        if(xpath != null && element != null)
+        {
+            Attribute nameAttribute = getAttributeByName("name");
+
+            if(nameAttribute != null)
+            {
+                xpathWithAttributes = String.format("%s[@name='%s']", xpathWithAttributes, nameAttribute.getValue());
+            }
+        }
+
+        return xpathWithAttributes;
     }
 
     protected Attribute getAttributeLinkedToAnchor(TreeElement anchor)
@@ -156,31 +224,6 @@ final class TreeElement
         }
 
         return null;
-    }
-
-    protected MapEntry<Position, Tree> getContainingTree(Element anchorElement)
-    {
-        Tree tree = new Tree();
-        Element rootElement = anchorElement;
-        TreeElement firstFound = null;
-
-        while(firstFound == null && rootElement != null && element != null)
-        {
-            tree = new Tree(rootElement);
-            firstFound = tree.getFirstMatchedTreeElement(this);
-
-            if(firstFound == null)
-            {
-                rootElement = rootElement.parent();
-
-                if(rootElement != null)
-                {
-                    rootElement.attr(tree.uniqueInsertedAttribute, UUID.randomUUID().toString());
-                }
-            }
-        }
-
-        return firstFound == null ? null : new MapEntry<>(firstFound.position, tree);
     }
 
     protected List<TreeElement> getAnchorsByShortestDistanceDepth(int shortestDistanceDepth)
@@ -301,21 +344,56 @@ final class TreeElement
         return rootPosition;
     }
 
-    @Override
-    public boolean equals(Object obj)
+    protected boolean matchElementOwnText(String pattern, Condition condition)
     {
-        if(obj == null || (obj != null && !(obj instanceof TreeElement)))
+        Condition activeCondition = condition;
+
+        if (condition == null)
+        {
+            activeCondition = new Condition();
+        }
+
+        if (element == null || element.ownText() == null || pattern == null)
         {
             return false;
         }
 
-        if(this.element == null || ((TreeElement) obj).element == null)
+        String elementOwnText = activeCondition.whereIncludingTabsAndSpacesForOwnText ?
+            element.ownText() :
+            element.ownText().replace("\\s+", "");
+        String patternWithoutSpaces = activeCondition.whereIncludingTabsAndSpacesForOwnText ?
+            pattern :
+            pattern.replace("\\s+", "");
+
+        if (activeCondition.whereIgnoreCaseForOwnText && activeCondition.whereOwnTextContainingPattern)
         {
-            return false;
+            if (elementOwnText.toLowerCase().contains(patternWithoutSpaces.toLowerCase()))
+            {
+                return true;
+            }
+        }
+        else if (activeCondition.whereIgnoreCaseForOwnText)
+        {
+            if (elementOwnText.equalsIgnoreCase(patternWithoutSpaces))
+            {
+                return true;
+            }
+        }
+        else if (activeCondition.whereOwnTextContainingPattern)
+        {
+            if (elementOwnText.contains(patternWithoutSpaces))
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (elementOwnText.equals(patternWithoutSpaces))
+            {
+                return true;
+            }
         }
 
-        return this.element.hasAttr(Tree.uniqueInsertedAttribute)
-               && ((TreeElement) obj).element.hasAttr(Tree.uniqueInsertedAttribute)
-               && this.element.attr(Tree.uniqueInsertedAttribute).equals(((TreeElement) obj).element.attr(Tree.uniqueInsertedAttribute));
+        return false;
     }
 }
