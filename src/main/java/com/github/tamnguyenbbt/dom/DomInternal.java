bb804928aeb7656f4651bec906cd89219bbb3d0f
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-19 13:28:38 +1100

    Update DomInternal.java

--- src/main/java/com/github/tamnguyenbbt/dom/DomInternal.java
+++ src/main/java/com/github/tamnguyenbbt/dom/DomInternal.java
@@ -335,7 +335,9 @@ class DomInternal extends DomCore
 
         if (Util.hasItem(elementsByLink))
         {
-            Elements anchorElements = getElementsByTagNameMatchingOwnText(document, anchorElementInfo.tagName, anchorElementInfo.ownText, anchorElementInfo.condition);
+            Elements anchorElements = anchorElementInfo.tagName == null 
+            ? getElementsMatchingOwnText(document, anchorElementInfo.ownText, anchorElementInfo.condition)
+            : getElementsByTagNameMatchingOwnText(document, anchorElementInfo.tagName, anchorElementInfo.ownText, anchorElementInfo.condition);
             Elements filteredAnchors = getElements(elementsByLink, anchorElements, SearchMethod.ByDistance, bestEffort);
             return getXpath(filteredAnchors, elementsByLink, SearchMethod.ByDistance, bestEffort);
         }
