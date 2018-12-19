0643495e7c978ca4c7a2b19f8dbfd836717d52a2
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-19 18:05:55 +1100

    Create HtmlTag.java

---
+++ src/main/java/com/github/tamnguyenbbt/dom/HtmlTag.java
@@ -0,0 +1,22 @@
+package com.github.tamnguyenbbt.dom;
+
+public enum HtmlTag
+{
+    a,
+    button,
+    div,
+    img,
+    input,
+    label,
+    li,
+    link,
+    option,
+    span,
+    table,
+    td,
+    textarea,
+    tr,
+    form,
+    select,
+    col
+}
