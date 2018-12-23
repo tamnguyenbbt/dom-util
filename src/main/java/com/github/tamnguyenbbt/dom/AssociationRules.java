ae68cc91ee798227e60469788073f95097c80775
Tam Nguyen <tamnguyenbbt@gmail.com>
2018-12-23 19:03:35 +1100

    Create AssociationRules.java

---
+++ src/main/java/com/github/tamnguyenbbt/dom/AssociationRules.java
@@ -0,0 +1,14 @@
+package com.github.tamnguyenbbt.dom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssociationRules
+{
+    public List<AssociationRule> associations;
+
+    public AssociationRules()
+    {
+        associations = new ArrayList<>();
+    }
+}
